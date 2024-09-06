0690bfe21640c40c2e25f1ce3535738aa543d638
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-09-06 16:58:29 +0000

    Finish Part 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,4 +2,7 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello!");
     }
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,6 +20,8 @@ public class Main extends Application {
 
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Duke duke = new Duke();
+
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -29,9 +31,6 @@ public class Main extends Application {
 
         userInput = new TextField();
         sendButton = new Button("Send");
-
-        DialogBox dialogBox = new DialogBox("Hello!", userImage);
-        dialogContainer.getChildren().addAll(dialogBox);
 
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
@@ -68,5 +67,27 @@ public class Main extends Application {
 
         AnchorPane.setLeftAnchor(userInput , 1.0);
         AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
     }
+    /**
+     * Creates a dialog box containing user input, and appends it to
+     * the dialog container. Clears the user input after processing.
+     */
+    private void handleUserInput() {
+        String userText = userInput.getText();
+        String dukeText = duke.getResponse(userInput.getText());
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, userImage),
+                DialogBox.getDukeDialog(dukeText, dukeImage)
+        );
+        userInput.clear();
+    }
+
 }
